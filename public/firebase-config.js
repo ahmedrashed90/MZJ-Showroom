d7b517bf6d2611109fe3307af74a1096420dbe72
ahmedrashed90 <134849433+ahmedrashed90@users.noreply.github.com>
2026-07-07 05:08:07 +0300

    Delete public directory

--- public/firebase-config.js
+++
@@ -1,9 +0,0 @@
-window.MZJ_FIREBASE_CONFIG = {
-  apiKey: "AIzaSyDC_KewSFylYjNzNQwGOnBqVA7QBorJT70",
-  authDomain: "mzj-showroom-screens.firebaseapp.com",
-  projectId: "mzj-showroom-screens",
-  storageBucket: "mzj-showroom-screens.firebasestorage.app",
-  messagingSenderId: "490543498446",
-  appId: "1:490543498446:web:44e429a420c5f33fab7c9f",
-  measurementId: "G-M7SM8G2MQC"
-};
